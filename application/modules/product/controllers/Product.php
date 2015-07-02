c78a9c754ec0167e92b81c2c498710db5a1d5f83
Mafaik <mafaikmon@gmail.com>
2015-07-02 08:05:45 +0700

    bugFixes2Juli2015

--- application/modules/product/controllers/Product.php
+++ application/modules/product/controllers/Product.php
@@ -16,17 +16,25 @@ class Product extends MX_Controller
         $this->parser->parse('index.tpl',$output);
     }
 
-    public function index()
+    public function index($id_product = null)
     {
         $crud = new grocery_CRUD();
+        if (!empty($id_product) && is_numeric($id_product)) {
+            $crud->where('product.parent', $id_product);
+            $crud->unset_operations();
+        } else {
+            $crud->add_action('Lihat Sub Produk', '', '', 'read-icon', array($this, 'addSubProductAction'));
+        }
 
         $crud->set_table('product')
-                ->columns('barcode', 'name', 'id_product_category', 'id_product_unit', 'brand', 'sell_price', 'date_expired', 'size', 'license', 'stock', 'minimum_stock')
+                ->columns('barcode', 'name', 'id_product_category', 'id_product_unit', 'value', 'brand', 'sell_price', 'date_expired', 'size', 'license', 'stock', 'minimum_stock')
                 ->display_as('id_product_category', 'Product Category')
-                ->display_as('id_product_unit', 'Product Unit')
+                ->display_as('id_product_unit', 'Product Satuan')
                 ->display_as('date_expired', 'Date Expired')
                 ->display_as('minimum_stock', 'Minimum Stock')
+                ->display_as('value', 'Nilai Satuan')
                 ->callback_column('sell_price', array($this, 'currencyFormat'))
+                ->callback_column('value', array($this, 'setUnitValue'))
                 ->set_relation('id_product_category', 'product_category', 'category')
                 ->set_relation('id_product_unit', 'product_unit', 'unit')
                 ->fields('barcode', 'id_product_category', 'parent', 'name', 'brand', 'id_product_unit', 'size', 'date_expired', 'license', 'minimum_stock')
@@ -34,6 +42,7 @@ class Product extends MX_Controller
                 ->unset_fields('weight', 'length', 'width', 'height', 'sell_price', 'stock')
                 ->unique_fields('barcode')
                 ->callback_field('parent', array($this, 'productParentField'))
+                ->callback_field('id_product_unit', array($this, 'setUnitField'))
                 ->unset_read();
 
         $output = $crud->render();
@@ -44,6 +53,12 @@ class Product extends MX_Controller
     public function currencyFormat($value, $row)
     {
         return "Rp " . number_format($value);
+    }
+
+    public function setUnitValue($value, $row)
+    {
+        $value = $this->ModProduct->getUnitValue($row->id_product_unit);
+        return $value;
     }
 
     public function productParentField($value = '', $primary_key = null)
@@ -61,21 +76,47 @@ class Product extends MX_Controller
         return $text;
     }
 
+    public function setUnitField($value = '', $primary_key = null)
+    {
+        $units = $this->ModProduct->getUnitOnly();
+        $text = '<select id="field-unit" name="id_product_unit" class="chosen-select chzn-done" data-placeholder="Select Parent" style="width: 300px;"><option value="">Select Parent</option>';
+        foreach ($units as $row) {
+            if ($value == $row['id_product_unit']) {
+                $text .= '<option value="' . $row['id_product_unit'] . '" selected>' . $row['unit'] . ' / ' . $row['value'] . '</option>';
+            } else {
+                $text .= '<option value="' . $row['id_product_unit'] . '">' . $row['unit'] . ' / ' . $row['value'] . '</option>';
+            }
+        }
+        $text .= '</select>';
+        return $text;
+    }
+
+    function addSubProductAction($value, $row)
+    {
+        return site_url('product/index') . '/' . $row->id_product;
+    }
+
     public function category($action = null, $id_product_category = null)
     {
         $this->session->set_userdata('selected_table', 'product_category');
         $crud = new grocery_CRUD();
+        if (!empty($id_product_category) && is_numeric($id_product_category)) {
+            $crud->where('parent', $id_product_category)
+                ->unset_operations();
+        } else {
+            $crud->add_action('Lihat Sub Produk Kategori', '', '', 'read-icon', array($this, 'addSubCategoryAction'));
+        }
+
         $crud->set_table('product_category')
-                ->columns('category', 'parent', 'prefix_code', 'note')
-                ->add_fields('category', 'prefix_code', 'parent', 'note')
-                ->edit_fields('category', 'prefix_code', 'parent', 'note')
+                ->columns('category', 'parent', 'note')
+                ->fields('category', 'parent', 'note')
                 ->required_fields('category')
                 ->display_as('category', 'Nama Kategori')
 //                ->callback_add_field('category', array($this, 'setPrefixCode'))
                 ->callback_field('note', array($this, 'setTextarea'))
 //                ->callback_add_field('prefix_code', array($this, 'disablePrefixCode'))
 //                ->callback_edit_field('prefix_code', array($this, 'disablePrefixCode'))
-                ->callback_before_insert(array($this, 'checkPrefixCode'))
+                // ->callback_before_insert(array($this, 'checkPrefixCode'))
                 ->callback_field('parent', array($this, 'categoryParentField'))
                 ->callback_column('parent', array($this, 'categoryParentName'))
                 ->unset_read();
@@ -107,16 +148,21 @@ class Product extends MX_Controller
         return $text;
     }
 
+    function addSubCategoryAction($value, $row)
+    {
+        return site_url('product/category/parent') . '/' . $row->id_product_category;
+    }
+
     public function unit() 
     {
         $this->session->set_userdata('selected_table', 'product_unit');
         $crud = new grocery_CRUD();
         $crud->set_table('product_unit')
-                ->columns('unit', 'prefix_code', 'value', 'note')
-                ->add_fields('unit', 'prefix_code', 'value', 'note')
-                ->edit_fields('unit', 'prefix_code', 'value', 'note')
+                ->display_as('unit', 'Satuan')
+                ->columns('unit', 'value', 'note')
+                ->fields('unit', 'value', 'note')
                 ->required_fields('unit', 'value', 'prefix_code')
-                ->display_as('prefix_code', 'Prefix Code')
+                // ->display_as('prefix_code', 'Prefix Code')
 //                ->callback_add_field('unit', array($this, 'setPrefixCode'))
                 ->callback_field('note', array($this, 'setTextarea'))
 //                ->callback_add_field('prefix_code', array($this, 'disablePrefixCode'))
