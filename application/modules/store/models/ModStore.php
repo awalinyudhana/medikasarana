8bdd17665411b63623895643af6b068b2e67eb8c
awalin yudhana <awalin.yudhana@nanomit.es>
2015-09-21 14:40:46 +0700

    nota so

---
+++ application/modules/store/models/ModStore.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ModStore extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getStoreDataById($id_store){
+        $this->db->where('id_store', $id_store);
+        $query = $this->db->get('store');
+
+        if ($query->num_rows() > 0) {
+            return $query->row_array();
+        }
+        return false;
+}
