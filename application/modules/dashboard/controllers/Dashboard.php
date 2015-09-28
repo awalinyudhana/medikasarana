b3ad5c23f88ebf3fb82fc95cc533afa5eff19755
awalin yudhana <awalin.yudhana@nanomit.es>
2015-09-28 12:23:06 +0700

    dashboard

--- application/modules/dashboard/controllers/Dashboard.php
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -28,7 +28,7 @@ class Dashboard extends MX_Controller
         $data['minimumStock'] = $this->ModDashboard->getMinimumStock();
         $data['expiredProducts'] = $this->ModDashboard->getExpiredProducts();
         $dataCredit = $this->ModDashboard->getCreditData();
-        $dataCreditBG = $this->ModDashboard->getCrediBGData();
+        $dataCreditBG = $this->ModDashboard->getCreditBGData();
         $dataDebit = $this->ModDashboard->getDebitData();
         $dataDebitBG = $this->ModDashboard->getDebitBGData();
 
