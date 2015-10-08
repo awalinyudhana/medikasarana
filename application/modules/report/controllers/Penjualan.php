c466ff9ef8a82d16e5612f9147ab535cda80403d
awalin yudhana <awalin.yudhana@nanomit.es>
2015-10-08 13:15:42 +0700

    report

--- application/modules/report/controllers/Penjualan.php
+++ application/modules/report/controllers/Penjualan.php
@@ -256,7 +256,7 @@ class Penjualan extends MX_Controller
                 'id_customer' => $object->id_customer,
                 'customer_name' => $object->id_customer,
                 'selling' => $selling
-            ]
+            ];
         }
 
 
