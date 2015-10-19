63868e3878ac63ab259211949d526ee9aedf6dc8
awalin yudhana <awalin.yudhana@nanomit.es>
2015-10-19 09:27:32 +0700

    bank info

--- application/modules/bank/controllers/Bank.php
+++ application/modules/bank/controllers/Bank.php
@@ -29,7 +29,7 @@ class Bank extends MX_Controller
                 ->display_as('bank_branch', 'Bank Branch')
                 ->set_relation('id_principal', 'principal', 'name')
                 ->fields('id_principal', 'bank_name', 'bank_account', 'bank_beneficiary_name', 'bank_city', 'bank_branch')
-                ->required_fields('id_principal', 'bank_name', 'bank_account')
+                ->required_fields('id_principal', 'bank_account', 'bank_beneficiary_name')
                 ->unset_read();
         $output = $crud->render();
         $this->render($output);
