8967ddb1a9cce19b5fec7e09478c2e2a25c3482d
Awalin Yudhana <otoy.destroyed@gmail.com>
2016-02-27 10:24:08 +0700

    mutasi

--- application/modules/report/models/ModMutasi.php
+++ application/modules/report/models/ModMutasi.php
@@ -15,7 +15,8 @@ class ModMutasi extends CI_Model
         }
 
         $this->db
-                ->select('pm.*, pu.*, pc.*,p.*, p.name AS product_name, pm.referral as name, pm.qty as mutasi_qty, pm.note as mutasi_note')
+                ->select('pm.*, pu.*, pc.*,p.*, p.name AS product_name, getNameMutasi(pm.referral,pm.note)
+                 , pm.qty as mutasi_qty, pm.note as mutasi_note', FALSE)
                 ->from('product_movement pm')
                 ->join('product p', 'p.id_product = pm.id_product')
                 ->join('product_unit pu', 'pu.id_product_unit = p.id_product_unit')
