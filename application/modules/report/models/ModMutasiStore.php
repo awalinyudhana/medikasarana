130e7da7bd2dfea74c4790e870a8100f2592b5b1
Awalin Yudhana <otoy.destroyed@gmail.com>
2016-02-28 23:24:17 +0700

    mutasi toko

--- application/modules/report/models/ModMutasiStore.php
+++ application/modules/report/models/ModMutasiStore.php
@@ -17,7 +17,8 @@ class ModMutasiStore extends CI_Model
         $this->db
                 ->select('pms.*, pu.*, pc.*,p.*, p.name AS product_name, pms.qty as mutasi_qty, pms.note as mutasi_note')
                 ->from('product_movement_store pms')
-                ->join('product p', 'p.id_product = pms.id_product')
+                ->join('product_store ps', 'ps.id_product_store = pms.id_product_store')
+                ->join('product p', 'p.id_product = ps.id_product')
                 ->join('product_unit pu', 'pu.id_product_unit = p.id_product_unit')
                 ->join('product_category pc', 'pc.id_product_category = p.id_product_category')
                 ->order_by('pms.datetime desc');
