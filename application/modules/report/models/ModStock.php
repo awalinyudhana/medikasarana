0ef5754d35a29660ee555bbf43744eb82990f21f
awalin yudhana <otoy.destroyed@gmail.com>
2016-05-10 19:03:41 +0700

    query report stok

--- application/modules/report/models/ModStock.php
+++ application/modules/report/models/ModStock.php
@@ -20,7 +20,7 @@ class ModStock extends CI_Model
     {
         $this->db->select('*, IF(ps.stock is null, 0, ps.stock) as stock_retail, p.stock as stock_warehouse', false);
         $this->db->from('product p');
-        $this->db->join('product_store ps', 'p.id_product = ps.id_product', 'left');
+        $this->db->join('product_store ps', 'p.id_product = ps.id_product', 'right');
         $this->db->join('product_unit pu', 'pu.id_product_unit = p.id_product_unit','left');
         $this->db->join('product_category pc', 'pc.id_product_category = p.id_product_category','left');
         $this->db->where(['id_store'=>$id_store]);
